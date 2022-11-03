336461a689b0ab587b0d579c4e1a2385dea52f37
misao <mi3aaao159@gmail.com>
2022-11-03 20:22:52 +0900

    add: contact page

---
+++ pages/contact-page.js
@@ -0,0 +1,32 @@
+import Layout from "../components/Layout";
+import { DlistItem } from "../components/atoms/listItem/DlistItem";
+import { UserIcon } from "../components/atoms/userIcon/UserIcon";
+import { SnsLinks } from "../components/Molecules/SnsLinks";
+
+const Contact = () => {
+  return (
+    <Layout title="Contact">
+      <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+        <div className="mt-4">
+          <h1 className="font-bold text-xl">Contact Info</h1>
+        </div>
+        <div className="flex justify-center mt-4">
+          <UserIcon
+            iconPath="/user-icon.png"
+            width="60"
+            height="60"
+            alt="apple"
+          />
+        </div>
+        <dl className="mt-4">
+          <DlistItem title="Address" text="city A" />
+          <DlistItem title="E-mail" text="abc@gmail.com" />
+          <DlistItem title="Phone" text="000-0000-0000" />
+        </dl>
+        <SnsLinks twLink="" fbLink="" ghLink="" />
+      </div>
+    </Layout>
+  );
+};
+
+export default Contact;
