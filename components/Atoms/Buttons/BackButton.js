9b374456f43a46c9f08bcc9f301f5ce258f3cd0d
misao <mi3aaao159@gmail.com>
2022-11-03 20:24:25 +0900

    add: posts page

---
+++ components/Atoms/Buttons/BackButton.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+const BackButton = (props) => {
+  const { url, text = "Back" } = props;
+  return (
+    <Link href={url}>
+      <div className="flex justify-center cursor-pointer mt-12">
+        <svg
+          className="w-6 h-6 mr-3"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+        <span>{text}</span>
+      </div>
+    </Link>
+  );
+};
+
+export default BackButton;
---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import BackButton from "../../components/Atoms/Buttons/BackButton";
+import Layout from "../../components/Layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+const Post = ({ post }) => {
+  if (!post) {
+    return <div>No article available</div>;
+  }
+  return (
+    <Layout title={post.title}>
+      <div className="w-9/12 m-auto">
+        <header className="mt-4">
+          <p className="text-center">ID: {post.id}</p>
+          <h1 className="mb-8 text-xl font-bold text-center">{post.title}</h1>
+        </header>
+        <div className="px-10">
+          <p>{post.body}</p>
+        </div>
+        <BackButton url="/blog-page/" text="Back to Blog" />
+      </div>
+    </Layout>
+  );
+};
+
+export default Post;
+
+export async function getStaticPaths() {
+  const paths = await getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const post = await getPostData(params.id);
+  return {
+    props: {
+      post,
+    },
+  };
+}
